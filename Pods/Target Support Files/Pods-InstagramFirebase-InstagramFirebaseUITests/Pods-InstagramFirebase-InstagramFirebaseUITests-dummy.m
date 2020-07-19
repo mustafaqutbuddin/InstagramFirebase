6723f422846f7b70b68cf015eeb36ca218a3972f
Mustafa <mq.kakajiwala@gmail.com>
2020-07-19 23:45:02 +0500

    Deleted pods

--- Pods/Target Support Files/Pods-InstagramFirebase-InstagramFirebaseUITests/Pods-InstagramFirebase-InstagramFirebaseUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_InstagramFirebase_InstagramFirebaseUITests : NSObject
-@end
-@implementation PodsDummy_Pods_InstagramFirebase_InstagramFirebaseUITests
-@end
--- Pods/Target Support Files/Pods-InstagramFirebaseTests/Pods-InstagramFirebaseTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_InstagramFirebaseTests : NSObject
-@end
-@implementation PodsDummy_Pods_InstagramFirebaseTests
-@end
